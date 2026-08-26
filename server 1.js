8decf8b62f6865e173327007f4e89acf671df502
Duda <bffgamers77@gmail.com>
2026-08-26 09:01:53 -0300

    feat: modelagem de Criaturas e Habitats

--- server 1.js
+++
@@ -1,7 +0,0 @@
-const readlineSync = require('realine-sync');
-let preco = Number(readlineSync.question("Digite o preço dos produtos: "))
-let imposto  = preco * 0.15;
-let precofinal = preco + imposto;
-console.log ("Preço inicial: R$", preco);
-console.log("Imposto(15%): R$", imposto);
-console.log("Preço final: R$", precofinal);
--- server 4.js
+++
@@ -1,9 +0,0 @@
-let notebook = {
-    nome: "Notebook Gamer",
-    preco: 4500,
-    especificacoes: [
-        "Intel Core i7",
-        "16GB RAM",
-        "512GB SSD"
-    ]
-};
